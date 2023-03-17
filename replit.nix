3537b9882875ea1ec3fd5ef89ba0d1a878f77c21
Ray Foss <git@vblip.com>
2023-03-17 13:47:33 +0900

    Replit nix package update

--- replit.nix
+++ replit.nix
@@ -1,8 +1,6 @@
 { pkgs }: {
-    deps = [
-        pkgs.bashInteractive
-        pkgs.nodejs
-        pkgs.nodePackages.vscode-langservers-extracted
-        pkgs.nodePackages.typescript-language-server
-    ];
+  deps = [
+    pkgs.nodejs-18_x
+      pkgs.nodePackages.typescript-language-server
+  ];
 }
